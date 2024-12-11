7277c0fab608321caf6283daf6240fba89c2d389
rizhou <you@example.com>
2024-12-11 14:51:49 +0800

    first commit

---
+++ src/CurlServiceProvider.php
@@ -0,0 +1,41 @@
+<?php namespace Ixudra\Curl;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class CurlServiceProvider extends ServiceProvider {
+
+    /**
+     * @var bool
+     */
+    protected $defer = false;
+
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('Curl', function () {
+                return new CurlService();
+            }
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function boot()
+    {
+        app(Builder::class)->enableStart();
+    }
+
+    /**
+     * @return array
+     */
+    public function provides()
+    {
+        return array('Curl');
+    }
+
+}
